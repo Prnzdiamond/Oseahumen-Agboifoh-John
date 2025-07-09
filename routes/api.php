f2b8167220d8bf57aa078039000d70ad09edbf93
Prnzdiamond <oseahumenagboifoh@gmail.com>
2025-07-09 10:36:56 +0100

    hi

--- routes/api.php
+++ routes/api.php
@@ -4,13 +4,13 @@ use App\Http\Controllers\OwnerController;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\ProjectController;
 
-Route::group(["middleware" => "validate.origin"], function () {
 
-    Route::get('projects', [ProjectController::class, 'index']);
-    Route::get('projects/{slug}', [ProjectController::class, 'show']);
+
+Route::get('projects', [ProjectController::class, 'index']);
+Route::get('projects/{slug}', [ProjectController::class, 'show']);
     // routes/api.php
     // routes/api.php
-    Route::post('/contact', [ContactController::class, 'send']);
-});
+Route::post('/contact', [ContactController::class, 'send']);
+
 
 Route::get('owner', [OwnerController::class, 'show']);
